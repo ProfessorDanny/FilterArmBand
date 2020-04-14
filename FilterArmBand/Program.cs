b0a73fb8752bbd0c3899d078d26009618d3bb267
Danny Lamb <51651615+ProfessorDanny@users.noreply.github.com>
2020-04-14 14:49:32 -0700

    Working Ver 1.0
    Fully functional
    std, SCV, and Canceled Visit

--- FilterArmBand/Program.cs
+++ FilterArmBand/Program.cs
@@ -1,4 +1,4 @@
-﻿// first LRS Filter program for Arm Bands
+﻿// LRS Filter program for Arm Bands
 // Basic function to cover printing the Arm Band
 
 
@@ -8,6 +8,7 @@ using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 
@@ -27,10 +28,13 @@ namespace FilterArmBand
             String OutputFileName;
             //String AttrFileName;
             String line;
+            String BarCodeVal = "";
             String TempFile;
             String term = "\r\n";
 
             string[] LabelLine = new string[14];
+            string[] BarCodeData = new string[2];
+            string Working1 = "";
             string ZPLString = "";
 
             //The filter definition should look like this
@@ -43,9 +47,9 @@ namespace FilterArmBand
             //AttrFileName = args[2];
 
 
-            //InputFileName = @"C:\test\Read4.txt";
-            //OutputFileName = @"C:\test\WriteText1.txt";
-            //AttrFileName = @"C:\test\AttrFileText.txt";
+            //InputFileName = @"D:\test\Read4.txt";
+            //OutputFileName = @"D:\test\WriteText1.txt";
+            //AttrFileName = @"D:\test\AttrFileText.txt";
 
 
             //  The following is how to post information to the LRS Printer Log
@@ -62,7 +66,7 @@ namespace FilterArmBand
             //  Console.WriteLine("This is what the Debug message type will look like");
 
             Console.WriteLine("<!VPSX-MsgType>DEBUG");
-            Console.WriteLine("Converting Schedule Label TXT to ZPL");
+            Console.WriteLine("Converting WristBand Data to ZPL");
 
             Console.WriteLine("<!VPSX-MsgType>INFO");
             Console.WriteLine("The Input Filename is {0}", InputFileName);
@@ -104,33 +108,33 @@ namespace FilterArmBand
             {
                 char[] charArr = line.ToCharArray();
 
-
-
                 if (line == "")
                 {
-                    int a;// System.Console.WriteLine(" BLANK Line ");
+                    int a;
+                    System.Console.WriteLine(" BLANK Line ");
                 }
                 else
                 {
                     if (line.Length < 2)
                     {
                         //Skip
-                        //Console.WriteLine("<!VPSX-MsgType>ERROR");
-                        // Console.WriteLine("Short Line Improper Data format Skip Line");
+                        Console.WriteLine("<!VPSX-MsgType>ERROR");
+                        Console.WriteLine("Short Line Improper Data format Skip Line");
                     }
                     else
                     {
                         //System.Console.WriteLine(line);
+
                         LabelLine[counter] = line;  // will cause error for wrong data  ie laser job
                         counter++;
 
                     }
 
 
-                    if (counter >= 12)
+                    if (counter >= 6)
                     {//ERROR
                         Console.WriteLine("<!VPSX-MsgType>ERROR");
-                        Console.WriteLine("More than 11 lines with no FF");
+                        Console.WriteLine("More than 6 lines with no *");
 
                     }
 
@@ -145,55 +149,144 @@ namespace FilterArmBand
                     // if (ch == 0x0A)
                     //     System.Console.WriteLine(" LF ");
 
-                    if (ch == 0x0C) // Form Feed found
+                    if (ch == 0x2A || ch == 0X60) // end of data has * or tic
                     {
+                        
 
-                        // Console.WriteLine("<!VPSX-MsgType>DEBUG");
-                        //  Console.WriteLine("Form Feed Found Write One ZPL Label to Output File");
+                        if (ch == 0x2A)
+                        {
+                            Console.WriteLine("<!VPSX-MsgType>DEBUG");
+                            Console.WriteLine("End of Data * Found Write One ZPL Label to Output File");
+                        }
 
+                        if (ch == 0x60)
+                        {
+                            Console.WriteLine("<!VPSX-MsgType>DEBUG");
+                            Console.WriteLine("End of Data TIC Found Write One ZPL Label to Output File");
+                        }
 
                         //System.Console.WriteLine(" Form Feed ");
                         //NewLabel = 1;  // FF end of label 
                         counter = 0;
+                        Working1 = LabelLine[1];
+                       // Console.WriteLine("******************");
+                        BarCodeData[0] = LabelLine[1].Substring(2, 8);
+                        BarCodeVal = LabelLine[1].Substring(2, 8);
+                        //Console.WriteLine(BarCodeVal);
+                        //Console.WriteLine("******************");
 
+                        //Console.Write("Line 3 :");
+                        //Console.WriteLine(LabelLine[3]);
+
+                       // Console.Write("Line 4 :");
+                        //Console.WriteLine(LabelLine[4]);
+
+                        if (ch == 0x60)
+                        {
+                            Console.WriteLine("Tic found");
+                            if (LabelLine[3].Length > 15)
+                            {
+                                Console.WriteLine("Length > 15");
+                                char[] charsToTrim = { '*', ' ', '\'' };
+                                //Working1 = LabelLine[3].Trim(charsToTrim);
+                                
+                                Working1 = LabelLine[3].Substring(36, 11);
+                                LabelLine[3] = " " + Working1;
+
+                            }
+
+
+                        }                        
+                        else
+                        {
+                            //if (counter > 3)
+                            {
+                                char[] charsToTrim = { '*', ' ' };
+                                Working1 = LabelLine[4].Trim(charsToTrim);
+                                LabelLine[4] = " " + Working1;
+                            }
+                        }
+
+                        /*
+                        Console.Write("Line 0 :"); 
+                        Console.WriteLine(LabelLine[0]);
+
+                        Console.Write("Line 1 :");
+                        Console.WriteLine(LabelLine[1]);
+
+                        Console.Write("Line 2 :");
+                        Console.WriteLine(LabelLine[2]);
+
+                        Console.Write("Line 3 :");
+                        Console.WriteLine(LabelLine[3]);
+
+                        Console.Write("Line 4 :");
+                        Console.WriteLine(LabelLine[4]);
+                        */
+                        // Working1 = LabelLine[5].Trim(charsToTrim);
+                        // LabelLine[5] = Working1;
+
+                        
 
                         // Data checks
 
-                        // Start Build ZPL Code for Schedule Label from data
+                        // Start Build ZPL Code for Adult2D WristBand from data
 
 
                         // WriteLabel();
 
-                        ZPLString =
-                        "^XA" + term +
-                        "^LH10,0" + term +
-                        "^FT20,38" + term +
-                        "^A0N,25,34^FD" + LabelLine[0] + "^FS" + term +
-                        "^FT20,68" +
-                        "^A0N,25,34^FD" + LabelLine[1] + "^FS" + term +
-                       "^FT20,98" +
-                        "^A0N,25,34^FD" + LabelLine[2] + "^FS" + term +
-                        "^FT20,128" +
-                        "^A0N,25,34^FD" + LabelLine[3] + "^FS" + term +
-                        "^FT20,158" +
-                        "^A0N,25,34^FD" + LabelLine[4] + "^FS" + term +
-                        "^FT20,188" +
-                        "^A0N,25,34^FD" + LabelLine[5] + "^FS" + term +
-                        "^FT20,218" +
-                        "^A0N,25,34^FD" + LabelLine[6] + "^FS" + term +
-                        "^FT20,248" +
-                        "^A0N,25,34^FD" + LabelLine[7] + "^FS" + term +
-                        "^FT20,278" +
-                        "^A0N,25,34^FD" + LabelLine[8] + "^FS" + term +
-                        "^FT20,308" +
-                        "^A0N,25,34^FD" + LabelLine[9] + "^FS" + term +
-                        "^FT20,338" +
-                        "^A0N,25,34^FD" + LabelLine[10] + "^FS" + term +
-                        "^FT20,368" +                                       // Added for data error in some formats extra line
-                        "^A0N,25,34^FD" + LabelLine[11] + "^FS" + term +    // extra line
-                        "^PQ1" +
-                        "^XZ" + term + term;
-                        // End Schedule Label
+
+                        // need to check BarCodeVal in case there is a Canceled Visit (make sure it is numeric)
+                        if (Regex.IsMatch(BarCodeVal, @"^\d+$"))
+                        {
+                            ZPLString =
+                            "^XA" + term +
+                            "^FT22,2200 ^A0B,30,36^FD" + LabelLine[0] + "^FS" + term +
+                            "^FT52,2200 ^A0B,30,36^FD" + LabelLine[1] + "^FS" + term +
+                            "^FT82,2200 ^A0B,30,36^FD" + LabelLine[2] + "^FS" + term +
+                            "^FT112,2200 ^A0B,30,36^FD" + LabelLine[3] + "^FS" + term +
+                            "^FT142,2200 ^A0B,30,36^FD" + LabelLine[4] + "^FS" + term +
+                            "^FO153,1895^BY2^BCB,51,N,N^FDAC" + BarCodeVal + "^FS" + term +  // Code 128 Linear 
+
+                            "^FO0,2230 ^BY4^BXB,4,200,14,14,4^FDAC" + BarCodeVal + "^FS" + term +  // Datamatrix code 1 of 4
+                            "^FO0,1545 ^BY4^BXB,4,200,14,14,4^FDAC" + BarCodeVal + "^FS" + term +  // Datamatrix code 2 of 4
+                            "^FO89,2230 ^BY4^BXB,4,200,14,14,4^FDAC" + BarCodeVal + "^FS" + term + // Datamatrix code 3 of 4
+                            "^FO89,1545 ^BY4^BXB,4,200,14,14,4^FDAC" + BarCodeVal + "^FS" + term + // Datamatrix code 4 of 4
+                            "^PQ1" +
+                            "^XZ ^MCY" + term + term;
+
+                            Console.WriteLine("<!VPSX-MsgType>DEBUG");
+                            Console.WriteLine("Write One ZPL Label to Output File with Barcodes");
+
+                        }
+                        else //  BarCodeVal in non numeric  E.G. Canceled Visit
+                        {
+                            ZPLString =
+                            "^XA" + term +
+                            "^FT22,2200 ^A0B,30,36^FD" + LabelLine[0] + "^FS" + term +
+                            "^FT52,2200 ^A0B,30,36^FD" + LabelLine[1] + "^FS" + term +
+                            "^FT82,2200 ^A0B,30,36^FD" + LabelLine[2] + "^FS" + term +
+                            "^FT112,2200 ^A0B,30,36^FD" + LabelLine[3] + "^FS" + term +
+                            "^FT142,2200 ^A0B,30,36^FD" + LabelLine[4] + "^FS" + term +
+
+                            //"^FO153,1895^BY2^BCB,51,N,N^FDAC" + BarCodeVal + "^FS" + term +  // Code 128 Linear 
+                            //"^FO0,2230 ^BY4^BXB,4,200,14,14,4^FDAC" + BarCodeVal + "^FS" + term +  // Datamatrix code 1 of 4
+                            //"^FO0,1545 ^BY4^BXB,4,200,14,14,4^FDAC" + BarCodeVal + "^FS" + term +  // Datamatrix code 2 of 4
+                            //"^FO89,2230 ^BY4^BXB,4,200,14,14,4^FDAC" + BarCodeVal + "^FS" + term + // Datamatrix code 3 of 4
+                            //"^FO89,1545 ^BY4^BXB,4,200,14,14,4^FDAC" + BarCodeVal + "^FS" + term + // Datamatrix code 4 of 4
+
+                            "^PQ1" +
+                            "^XZ ^MCY" + term + term;
+
+                            Console.WriteLine("<!VPSX-MsgType>DEBUG");
+                            Console.WriteLine("Canceled Visit  Write One ZPL Label to Output File with no Barcodes");
+
+
+                        }
+
+
+
+                        // End Wrist Band Print
 
 
                         using (StreamWriter sw = File.AppendText(OutputFileName))
@@ -222,12 +315,12 @@ namespace FilterArmBand
             // close the file?
             file.Close();
 
-            File.Delete(TempFile);
+            //File.Delete(TempFile);
 
             Console.WriteLine("<!VPSX-MsgType>DEBUG");
-            Console.WriteLine("Done Close File and Delete:");
+            Console.Write("Done Close Working File and Delete:");
 
-            Console.WriteLine("<!VPSX-MsgType>DEBUG");
+            //Console.WriteLine("<!VPSX-MsgType>DEBUG");
             Console.WriteLine(TempFile);
 
             // System.Console.WriteLine("There were {0} lines.", counter);
@@ -251,49 +344,7 @@ namespace FilterArmBand
 
 
         }
-        /*
-        public static void WriteLabel()
-        {
-            int i;
 
-            ZPLString =
-            "^XA" +
-            "^LH10,0" +
-            "^FT20,38" +
-            "^A0N,25,34^FD" + Program.LabelLine[0] + "^FS" +
-            "^FT20,68" +
-            "^A0N,25,34^FD" + LabelLine[1] + "^FS" +
-            "^FT20,98" +
-            "^A0N,25,34^FD" + LabelLine[2] + "^FS" +
-            "^FT20,128" +
-            "^A0N,25,34^FD" + LabelLine[3] + "^FS" +
-            "^FT20,158" +
-            "^A0N,25,34^FD" + LabelLine[4] + "^FS" +
-            "^FT20,188" +
-            "^A0N,25,34^FD" + LabelLine[5] + "^FS" +
-            "^FT20,218" +
-            "^A0N,25,34^FD" + LabelLine[6] + "^FS" +
-            "^FT20,248" +
-            "^A0N,25,34^FD" + LabelLine[7] + "^FS" +
-            "^FT20,278" +
-            "^A0N,25,34^FD" + LabelLine[8] + "^FS" +
-            "^FT20,308" +
-            "^A0N,25,34^FD" + LabelLine[9] + "^FS" +
-            "^FT20,338" +
-            "^A0N,25,34^FD" + LabelLine[10] + "^FS" +
-            "^PQ1" +
-            "^XZ" + 0x0C;
-
-           // using (StreamWriter w = File.AppendText(OutputFileName))
-          //  {
-          //      w.WriteLine(ZPLString);
-         //   }
-
-          //  System.IO.File.AppendText
-
-
-        }
-        */
     }
 
 
